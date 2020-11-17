8b72b24cef21579a25e611a4887ed513047f0f16
Victor Garcia Mestre <vgmestre@gmail.com>
2020-11-17 22:48:39 +0100

    added comment on dependent option

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ApplicationRecord
-  has_one_attached :thumbnail
+  has_one_attached :thumbnail # If the :dependent option isn't set, all the attachments will be purged (i.e. destroyed) whenever the record is destroyed
   belongs_to :platform
 end
