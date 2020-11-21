93075d4ff12dc7d4cae69b6e33213d2de41a422b
Victor Garcia Mestre <vgmestre@gmail.com>
2020-11-21 11:18:13 +0100

    capitalize env vars to correct active storage bug

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV["CLOUD_NAME"]
-  config.api_key = ENV["API_KEY"]
-  config.api_secret = ENV["API_SECRET"]
-  config.enhance_image_tag = ENV["ENHANCE_IMAGE_TAG"]
-  config.static_file_support = ENV["STATIC_FILE_SUPPORT"]
+  config.cloud_name = ENV['CLOUD_NAME']
+  config.api_key = ENV['API_KEY']
+  config.api_secret = ENV['API_SECRET']
+  config.enhance_image_tag = ENV['ENHANCE_IMAGE_TAG']
+  config.static_file_support = ENV['STATIC_FILE_SUPPORT']
 end
