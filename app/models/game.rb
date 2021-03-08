d9701667c579ed99a467b3775135d0cf113baae9
Victor Garcia Mestre <vgmestre@gmail.com>
2021-03-08 14:23:16 +0100

    stylised form components

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
   has_one_attached :thumbnail # If the :dependent option isn't set, all the attachments will be purged (i.e. destroyed) whenever the record is destroyed
   belongs_to :platform
+  validates :title, :platform, presence: true
 end
